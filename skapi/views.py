2ccb9cbf4ad5667bc887989ff62c567d80787b43
haiquan.ye <haiquan.ye@163.com>
2017-08-25 18:48:44 +0800

    no message

--- skapi/views.py
+++ skapi/views.py
@@ -3,6 +3,7 @@ import json
 
 from django.http import HttpResponse
 from skapi.models import Applist
+from skcmdb.models import App
 
 
 def grafana(request):
@@ -19,10 +20,9 @@ def grafana_search(request):
         target_data = json_data['target']
         method = json.loads(target_data).get('method','')
     if method == 'appname':
-        applist = Applist.objects.filter(is_active=1)
+        applist = App.objects.filter(status=1)
         data = [{'text': i.name, 'value': i.name} for i in applist]
         return HttpResponse(json.dumps(data),content_type="application/json")
     else:
         return HttpResponse("args error")
-
 
