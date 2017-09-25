54d6da360e303a282c454675ef1b223a9d4f5f8e
zeal <liang.zhang02@mljr.com>
2017-09-25 11:38:23 +0800

    合并代码

--- skrpt/views.py
+++ skrpt/views.py
@@ -7,14 +7,17 @@ from django.core.urlresolvers import reverse
 from .models import Rpt
 from forms import Rpt_form
 from skaccounts.models import UserInfo
+from skcmdb.models import Host, App
 @login_required()
 @permission_verify()
 def index(request):
     temp_name = "skrpt/navi-header.html"
     test01 = Rpt.objects.filter(app__in=[55]).count()
     test02 = Rpt.objects.filter(app__in=[5]).count()
-    user = UserInfo.objects.all().count()
-
+    usernum = UserInfo.objects.all().count()
+    phynum = Host.objects.filter(group_id__in=[1]).count()
+    vmnum = Host.objects.filter(group_id__in=[2]).count()
+    appnum = user = App.objects.all().count()
     return render_to_response("skrpt/index.html", locals(), RequestContext(request))
 
 
