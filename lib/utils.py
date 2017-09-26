916cb82f8bceeae4a348395f17fe6064096c290a
haiquan.ye <haiquan.ye@163.com>
2017-09-26 12:40:14 +0800

    no message

--- lib/utils.py
+++ lib/utils.py
@@ -1,9 +1,11 @@
 #!/url/bin/env python
 #-*- coding: utf8  -*-
 
+from django.conf import settings
 import MySQLdb
-
 from skcmdb.models import DbSource
+import ConfigParser
+
 
 ASSET_TYPE = (
     (1, u"物理机"),
@@ -27,6 +29,31 @@ def mysql_execute(dbsource,sql):
     cursor.close()
     db.close()
     return data
+
+#
+# class config:
+#     def __init__(self,):
+#         pass
+#
+#     config = ConfigParser.ConfigParser()
+#     with open(dirs + '/skipper.conf', 'r') as cfgfile:
+#         config.readfp(cfgfile)
+#         a_path = config.get('config', 'ansible_path')
+#         pro_path = config.get('config', 'project_base_path')
+#         git_path = config.get('config', 'git_base_path')
+#
+#         #         r_path = config.get('config', 'roles_path')
+#         #         p_path = config.get('config', 'playbook_path')
+#         #         s_path = config.get('config', 'scripts_path')
+#         engine = config.get('db', 'engine')
+#         host = config.get('db', 'host')
+#         port = config.get('db', 'port')
+#         user = config.get('db', 'user')
+#         password = config.get('db', 'password')
+#         database = config.get('db', 'database')
+#         token = config.get('token', 'token')
+#         log_path = config.get('log', 'log_path')
+#         log_level = config.get('log', 'log_level')
 
 
 
