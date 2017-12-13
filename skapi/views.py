b8e6588f988fd6a021b3c03736fbf5df2f013db8
haiquan.ye <haiquan.ye@163.com>
2017-12-13 19:04:39 +0800

    nginx 监控数据调优

--- skapi/views.py
+++ skapi/views.py
@@ -39,33 +39,46 @@ def grafana_search(request):
 
 
 def zabbix_sender(request):
+    log = logging.getLogger('nginx_info')
     zabbbix_server = request.POST.get('zabbbix_server', '10.8.48.211')
     agent_ip = request.POST.get('agent_ip', '')
     data = request.POST.get('data', '')
     if data and agent_ip:
         datas = data.split('\n')
         count = 0
+        data_dict = {}
         for data in datas:
             if data:
                 _data = data.split(',')
                 url = _data[0].split('-')[0]
                 pay_loads = {
-                    'bytes_in': _data[1],
-                    'bytes_out': _data[2],
-                    'http_2xx': _data[5],
-                    'http_3xx': _data[6],
-                    'http_4xx': _data[7],
-                    'http_5xx': _data[8],
-                    'rt': _data[10]
+                    'bytes_in': int(_data[1]),
+                    'bytes_out': int(_data[2]),
+                    'http_2xx': int(_data[5]),
+                    'http_3xx': int(_data[6]),
+                    'http_4xx': int(_data[7]),
+                    'http_5xx': int(_data[8]),
+                    'rt': int(_data[10]),
                 }
-                for k in pay_loads:
-                    cmd = '''/usr/bin/zabbix_sender -s "%s" -z "%s" -k url[%s,%s] -o "%s"''' % (
-                    agent_ip, zabbbix_server, url, k, pay_loads[k])
-                    code, result = commands.getstatusoutput(cmd)
-                    if code != 0:
-                        count += 1
-                        # msg = 'error:' + url + ',' + k + ',' + pay_loads[k]
-                        # logging.info(msg)
+                if url in data_dict:
+                    data_dict[url]['bytes_in'] += pay_loads['bytes_in']
+                    data_dict[url]['bytes_out'] += pay_loads['bytes_out']
+                    data_dict[url]['http_2xx'] += pay_loads['http_2xx']
+                    data_dict[url]['http_3xx'] += pay_loads['http_3xx']
+                    data_dict[url]['http_4xx'] += pay_loads['http_4xx']
+                    data_dict[url]['http_5xx'] += pay_loads['http_5xx']
+                    data_dict[url]['rt'] += pay_loads['rt']
+                else:
+                    data_dict[url] = pay_loads
+        for url in data_dict:
+            for key in data_dict[url]:
+                cmd = '''/usr/bin/zabbix_sender -s "%s" -z "%s" -k url[%s,%s] -o "%s"''' % (
+                agent_ip, zabbbix_server, url, key, data_dict[url][key])
+                code, result = commands.getstatusoutput(cmd)
+                if code != 0:
+                    count += 1
+                    msg = 'error:' + url + ',' + key + ',' + str(data_dict[url][key])
+                    log.info(msg)
         if count == 0:
             return HttpResponse('ok')
         else:
