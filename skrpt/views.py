beb81540d4f9b896d19a379271543bca4df083d0
zeal <liang.zhang02@mljr.com>
2017-12-28 10:29:34 +0800

    ADD report ichart

--- skrpt/views.py
+++ skrpt/views.py
@@ -8,6 +8,12 @@ from .models import Rpt
 from forms import Rpt_form
 from skaccounts.models import UserInfo
 from skcmdb.models import Host, App
+from sktask.models import history
+import  datetime
+import time
+from django.db.models import Q
+
+
 @login_required()
 @permission_verify()
 def index(request, *args, **kwargs):
@@ -16,6 +22,87 @@ def index(request, *args, **kwargs):
     phynum = Host.objects.filter(group_id__in=[1]).count()
     vmnum = Host.objects.filter(group_id__in=[2]).count()
     appnum = user = App.objects.all().count()
+
+       #sktask-total
+
+    date_list = []
+    num_list = []
+    cc = ""
+    dd = ""
+    if request.method == "POST":
+       a = request.POST['startDate']
+       b = request.POST['endDate']
+       now  = datetime.datetime.now()
+       now_date = str(datetime.date(now.year,now.month,now.day))
+       if b > now_date:
+           b = now_date
+       cc = a
+       dd = b
+       if cc >= dd:
+           nnn = 1
+       else:
+           nnn = 0
+       startDateArr=a.split("-")
+       startDateArr=map(int,startDateArr)
+       endDateArr=b.split("-")
+       endDateArr=map(int,endDateArr)
+       begin = datetime.date(startDateArr[0], startDateArr[1], startDateArr[2])
+       end = datetime.date(endDateArr[0], endDateArr[1], endDateArr[2])
+       d = begin
+       delta = datetime.timedelta(days=1)
+       if b > a:
+           while d <= end:
+             m = d.strftime("%Y-%m-%d")
+             daynum = history.objects.filter(time_task_finished__contains=m).count()
+             print("a>b:",daynum)
+             date_list.append(m)
+             num_list.append(daynum)
+             d += delta
+
+       else:
+           b = datetime.date.today()
+           a = b - datetime.timedelta(days=21)
+           datediff = (b - a).days
+           print(datediff)
+           a = str(a)
+           b = str(b)
+           startDateArr = a.split("-")
+           startDateArr = map(int, startDateArr)
+           endDateArr = b.split("-")
+           endDateArr = map(int, endDateArr)
+           begin = datetime.date(startDateArr[0], startDateArr[1], startDateArr[2])
+           end = datetime.date(endDateArr[0], endDateArr[1], endDateArr[2])
+           d = begin
+           delta = datetime.timedelta(days=1)
+           while d <= end:
+               m = d.strftime("%Y-%m-%d")
+               daynum = history.objects.filter(time_task_finished__contains=m).count()
+               print("a<b:",daynum)
+               date_list.append(m)
+               num_list.append(daynum)
+               d += delta
+    else:
+        b = datetime.date.today()
+        a = b - datetime.timedelta(days=21)
+        datediff = (b - a).days
+        a = str(a)
+        b = str(b)
+        startDateArr = a.split("-")
+        startDateArr = map(int, startDateArr)
+        endDateArr = b.split("-")
+        endDateArr = map(int, endDateArr)
+        begin = datetime.date(startDateArr[0], startDateArr[1], startDateArr[2])
+        end = datetime.date(endDateArr[0], endDateArr[1], endDateArr[2])
+        d = begin
+        delta = datetime.timedelta(days=1)
+        while d <= end:
+            m = d.strftime("%Y-%m-%d")
+            daynum = history.objects.filter(time_task_finished__contains=m).count()
+            date_list.append(m)
+            num_list.append(daynum)
+            d += delta
+
+
     return render_to_response("skrpt/index.html", locals(), RequestContext(request))
 
 
---
+++ static/js/showchart.js
@@ -0,0 +1,116 @@
+$(function(){
+            var flow =  num_list ;
+             var labels2 =  date_list;
+             var labels = new Array()
+             var temp = new Array()
+
+             for(var i=0; i<labels2.length;i++){
+                temp=labels2[i].split("-")
+                 labels[i] = temp[1]+"-"+temp[2]+'\n' + temp[0]
+             }
+
+
+
+             var data = [
+			         	{
+			         		name : '运维任务数量统计表',
+			         		value:flow,
+			         		<!--color:'#ec4646',-->
+						    color:'#0cff9c',
+			         		line_width:4
+			         	}
+			         ];
+
+
+
+			var chart = new iChart.LineBasic2D({
+				render : 'canvasDiv',
+				data: data,
+				align:'center',
+				title : {
+					text:'运维任务数量统计表',
+					font : '微软雅黑', //字体
+					fontsize:00, //字体大小设置
+					color:'#000'
+				},
+
+
+				width : 1450,
+				height : 400,
+				shadow:false,
+				shadow_color : '#fff',
+				shadow_blur : 0,
+				shadow_offsetx : 0,
+				shadow_offsety : 0,
+                background_color:'#ffffff',
+
+				tip:{
+					enable:true,
+					shadow:true,
+					listeners:{
+						 //tip:提示框对象、name:数据名称、value:数据值、text:当前文本、i:数据点的索引
+						parseText:function(tip,name,value,text,i){
+							return "<span style='color:#000000;font-size:12px;'>"+labels[i]+"日任务执行数为:<br/>"+
+							"</span><span style='color:#000000;font-size:20px;'>"+value+"个</span>";
+						}
+					}
+				},
+				sub_option : {
+					smooth : true,
+					label:false,
+					hollow:false,
+					hollow_inside:false,
+					point_size:8
+				},
+				coordinate:{
+					width:1350,
+					height:260,
+					striped_factor : 0.18,
+
+                    grid_line_width:0.7,
+					// grid_color:'#ffffff',
+					axis:{
+						color:'#fff',
+						width:[0,0,0,0]
+					},
+					scale:[{
+						 position:'left',
+						 start_scale:0,
+						 end_scale:8,
+						 scale_space:4,
+						 scale_size:1000,
+						 scale_enable : true,
+						 // label : {color:'#000000',font : '微软雅黑',fontsize:12,fontweight:600},
+						 scale_color:'#fff'
+					},{
+						 position:'bottom',
+						 // label : {color:'#000000',font : '微软雅黑',fontsize:12,fontweight:600},
+						 scale_enable : false,
+						 labels:labels
+					}]
+				}
+			});
+			// //利用自定义组件构造左侧说明文本
+			// chart.plugin(new iChart.Custom({
+			// 		drawFn:function(){
+			// 			//计算位置
+			// 			var coo = chart.getCoordinate(),
+			// 				x = coo.get('originx'),
+            //
+			// 				y = coo.get('originy'),
+			// 				w = coo.width,
+			// 				h = coo.height;
+            //
+			// 			//在左上侧的位置，渲染一个单位的文字
+			// 			chart.target.textAlign('start')
+			// 			.textBaseline('bottom')
+			// 			.textFont('600 11px 微软雅黑')
+			// 			// .fillText('任务数（单位个)',x-40,y-12,false,'#000000')
+			// 			.textBaseline('top')
+            //
+            //
+			// 		}
+			// }));
+		//开始画图
+		chart.draw();
+	});
