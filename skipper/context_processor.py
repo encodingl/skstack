f5f5557d2b9109a91256553566327f03fbe7d77e
haiquan.ye <haiquan.ye@163.com>
2017-12-28 22:42:20 +0800

    优化

--- skipper/context_processor.py
+++ skipper/context_processor.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 def url_permission(request):
+    url_permission_list = []
+    permission_flag = False
     if request.user.username and request.user.role:
         url_permission = request.user.role.permission.values_list('url')
         url_permission_list = [u[0] for u in url_permission]
-    else:
-        url_permission_list  = ['/all']
-    return {'url_permission_list': url_permission_list}
-
+        if '/' in url_permission_list:
+            permission_flag = True
+    elif request.user.is_superuser:
+        permission_flag = True
+    return {'url_permission_list': url_permission_list, 'permission_flag': permission_flag}
