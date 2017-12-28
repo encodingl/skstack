f50c483d07bd0fa345826ab61adb66066d60efcd
haiquan.ye <haiquan.ye@163.com>
2017-12-28 20:47:29 +0800

    no message

--- skipper/context_processor.py
+++ skipper/context_processor.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 def url_permission(request):
-    if request.user.role:
+    if request.user.username and request.user.role:
         url_permission = request.user.role.permission.values_list('url')
         url_permission_list = [u[0] for u in url_permission]
     else:
