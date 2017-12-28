2325fcc52ed135ef72b58716aae6ccde701b7163
haiquan.ye <haiquan.ye@163.com>
2017-12-28 18:41:39 +0800

    修改名称

--- skipper/context_processor.py
+++ skipper/context_processor.py
@@ -3,7 +3,7 @@
 
 def url_permission(request):
     if not request.user.is_superuser:
-        url_permission = request.user.role.permission.values_list('name')
+        url_permission = request.user.role.permission.values_list('url')
         url_permission_list = [u[0] for u in url_permission]
     else:
         url_permission_list  = ['/all']
