f5641cad47d6e98b5cf863af72ffec1894cfc12b
encodingl <encodingl@sina.com>
2019-01-24 18:14:23 +0800

    test02

--- files.py
+++ files.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
-
 import os
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render_to_response,redirect,RequestContext
