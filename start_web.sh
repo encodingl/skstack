b419d080e46852db516a8556c93e9e4d91564cbc
logan.li <logan.li@hwtech.hk>
2019-08-16 11:18:09 +0800

    ci13

--- start_web.sh
+++ start_web.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 stdout_logfile=/opt/sklogs/skipper/skipper.log
-python manage.py runserver 0.0.0.0:8000 > $stdout_logfile 2>&1 
+python manage.py runserver 0.0.0.0:8000 > $stdout_logfile 2>&1 &
